7d9402a55687110ae9d0c5314b340c50df65eae6
Martin Mariño <martin.mbosco@web-experto.com.ar>
2017-06-29 14:36:42 -0300

    Modificacion a nueva forma sidebar de contacto

--- app/overrides/add_newsletters_to_admin_sidebar_menu.rb
+++ app/overrides/add_newsletters_to_admin_sidebar_menu.rb
@@ -1,6 +1,5 @@
-Deface::Override.new(
-  virtual_path: 'spree/layouts/admin',
-	name: 'newsletter_admin_sidebar_menu',
-	insert_bottom: '#main-sidebar',
-	partial: 'spree/admin/shared/newsletter_sidebar_menu'
-)
+Deface::Override.new(:virtual_path => 'spree/admin/shared/_contacts',
+                     :name => 'add_newsletters_to_contacts_sidebar',
+                     :insert_bottom => '#sidebar-contacts',
+                     :partial => 'spree/admin/shared/newsletter_sidebar_menu'
+                    )
