adc673362e9ad1c56364e4044a787960dfe8c155
Martin Acevedo <macevedo@web-experto.com.ar>
2017-09-27 17:34:46 -0300

    Posible fix del error

--- app/assets/javascripts/spree/frontend/spree_newsletter.js
+++ app/assets/javascripts/spree/frontend/spree_newsletter.js
@@ -2,18 +2,20 @@
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/frontend/all.js'
 
 function submitNewsletter(selector) {
-	$.ajax({
-		type: "POST",
-		dataType: 'JSON',
-		url: $(selector).attr("action"),
-		data: $(selector).serialize(),
-		success: function(data) {
-			if (data.status === "ok") {
-				alert(data.msj);
-			}
-		},
-		error: function(data) {
-			alert(data);
-		}
-	})
+    $.ajax({
+        type: "POST",
+        dataType: 'JSON',
+        url: $(selector).attr("action"),
+        data: $(selector).serialize(),
+        success: function(data) {
+            if (data.status === "ok") {
+                alert(data.msj);
+            }
+        },
+        error: function(data) {
+            alert(data);
+        }
+    });
+
+    return false;
 }
