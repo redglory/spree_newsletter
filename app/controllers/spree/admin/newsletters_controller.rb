359d0f61672d7341c808360d5d3ac88e6c3c29a6
Juan M. Rinaudo <juanmanuel.rinaudo@web-experto.com.ar>
2018-06-06 13:40:05 -0300

    Se agrega opcion para exportar lista de usuarios del newsletter.

--- app/controllers/spree/admin/newsletters_controller.rb
+++ app/controllers/spree/admin/newsletters_controller.rb
@@ -1,18 +1,21 @@
 module Spree
   module Admin
-		class NewslettersController < ResourceController
-			def index
-				@newsletters = Spree::Newsletter.all
-		 	end
+    class NewslettersController < ResourceController
+      def index
+        respond_to do |format|
+          format.html { Spree::Newsletter.all }
+          format.csv { send_data Spree::Newsletter.export_csv, filename: "usuarios_boletin-#{Date.today}.csv" }
+        end
+      end
 
-			def delete
-				Spree::Newsletter.find(params[:id]).destroy
-				redirect_to :action => 'index'
-			end
+      def delete
+        Spree::Newsletter.find(params[:id]).destroy
+        redirect_to action: 'index'
+      end
 
-			def newsletter_params
-				params.require(:newsletter).permit(:email)
-			end
+      def newsletter_params
+        params.require(:newsletter).permit(:email)
+      end
     end
   end
 end
--- app/overrides/add_newsletters_to_admin_sidebar_menu.rb
+++ app/overrides/add_newsletters_to_admin_sidebar_menu.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path: 'spree/layouts/admin',
-	name: 'newsletter_admin_sidebar_menu',
-	insert_bottom: '#main-sidebar',
-	partial: 'spree/admin/shared/newsletter_sidebar_menu'
+  name: 'newsletter_admin_sidebar_menu',
+  insert_bottom: '#main-sidebar',
+  partial: 'spree/admin/shared/newsletter_sidebar_menu'
 )
