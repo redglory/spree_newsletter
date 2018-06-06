82660d5b91caa5b4e954a3f907da93a57db43dfc
Juan M. Rinaudo <juanmanuel.rinaudo@web-experto.com.ar>
2018-06-06 13:40:05 -0300

    Se agrega opcion para exportar lista de usuarios del newsletter.

---
+++ spec/newsletters_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe 'Newsletters' do
+  context 'as admin user' do
+    stub_authorization!
+
+    context 'listing newsletter users' do
+      it 'shows an export button' do
+        visit spree.admin_newsletters_path
+        expect(page).to have_selector(:css, '#btn-export')
+      end
+    end
+  end
+end
